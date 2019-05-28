685b2531ed4398142291d865cea58a036057d47d
gattygamaun <gattygamaun@gmail.com>
2019-05-28 16:10:04 +0300

    feat: add srcset for main image, make menu absolute

--- src/components/main-img/MainImg.js
+++ src/components/main-img/MainImg.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import slider from './slider.jpg'
+import large from './img/main-img-desktop.jpg';
+// import large from './img/photo-3.jpg';
+import medium from './img/main-img-tablet.jpg';
+// import medium from './img/photo-1.jpg';
+import small from './img/main-img-mobile.jpg';
+// import small from './img/photo-2.jpg';
+import './MainImg.scss';
 
-function MainImg() {
-  return (
-    <div>
-      <img src={slider} alt="Jesus in the coffin" className="main-img"/>
-    </div>
-  )
+class MainImg extends React.Component {
+  render () {
+    return (
+      <div>
+        <img src={large}
+             srcSet={`${large} 1200w, ${medium} 850w, ${small} 500w`}
+             alt="Jesus in the coffin" className="main-img"/>
+      </div>
+    )
+  }
 }
 
 export default MainImg;
