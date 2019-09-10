777ed2991d01a6316d01537cb3f11c695b5dd753
Sergei Kondratiuk <siarhei_kandratsiuk@epam.com>
2019-09-10 15:03:28 +0300

    fix: change display for mobile menu

--- src/layout/navigation/index.js
+++ src/layout/navigation/index.js
@@ -4,19 +4,16 @@ import { NavLink } from 'react-router-dom';
 import { mainLinks } from '../../data/about';
 
 class Navigation extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isToggleOn: true };
 
-    // This binding is necessary to make `this` work in the callback
-    this.handleClick = this.handleClick.bind(this);
-  }
+  state = {
+    isToggleOn: true
+  };
 
-  handleClick() {
+  handleMenuOpen = () => {
     this.setState(state => ({
       isToggleOn: !state.isToggleOn,
     }));
-  }
+  };
 
   render() {
     return (
@@ -25,7 +22,7 @@ class Navigation extends Component {
           ? 'hamburger hamburger--3dy' : 'hamburger hamburger--3dy is-active'}
                 type="button"
                 id="menu-toggle"
-                onClick={this.handleClick}>
+                onClick={this.handleMenuOpen}>
           <span className="hamburger-box">
             <span className="hamburger-inner"/>
           </span>
@@ -33,11 +30,11 @@ class Navigation extends Component {
         <ul className={this.state.isToggleOn ? 'menu-list' : 'menu-list is-active'}>
           {Object.entries(mainLinks).map(link => {
             return (<li className="menu-list-item" key={link[0]}>
-                      <NavLink to={(link[0] === 'schedule') ? '/' : `/${link[0]}`}
-                               activeStyle={{ fontWeight: 'bolder' }}>
-                        {link[1]}
-                      </NavLink>
-                    </li>);
+              <NavLink to={(link[0] === 'schedule') ? '/' : `/${link[0]}`}
+                       activeStyle={{ fontWeight: 'bolder' }}>
+                {link[1]}
+              </NavLink>
+            </li>);
           })}
         </ul>
       </nav>
