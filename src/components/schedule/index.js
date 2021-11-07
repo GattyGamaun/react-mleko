6e545ee24f3b179121931e4ba18dfcbd7b46d25f
Siarhei Kandratsiuk <siarhei.kandratsiuk@epam.com>
2021-11-07 17:45:47 +0300

    feat: add alternate text for schedule

--- src/components/schedule/index.js
+++ src/components/schedule/index.js
@@ -9,7 +9,7 @@ class Schedule extends Component {
         <figcaption>
           <h3>{schedule.title}</h3>
         </figcaption>
-        <img src={`${schedule.url}?${this.getDay()}`} alt="Расписание" />
+        <img src={`${schedule.url}?${this.getDay()}`} alt="Актуальное расписание в Viber в группе Млекопитательница" />
         <p>{schedule.note}</p>
         <p className="updated">{this.getDate()}</p>
       </figure>
