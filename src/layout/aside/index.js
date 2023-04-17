b27c2db3dae45fc94ea1457898e6719a5a9a0720
Siarhei Kandratsiuk <siarhei_kandratsiuk@epam.com>
2023-04-17 16:40:18 +0200

    add qr code

--- src/layout/aside/index.js
+++ src/layout/aside/index.js
@@ -7,37 +7,28 @@ function Aside() {
     <aside>
       <Fragment>
         <figure>
-          <img src={aside.images.mleko.src} alt={aside.images.mleko.alt}/>
+          <img src={aside.images.mleko.src} alt={aside.images.mleko.alt} />
           <figcaption>{aside.images.mleko.title}</figcaption>
         </figure>
       </Fragment>
       <Fragment>
         <figure>
-          <img src={aside.images.head.src} alt={aside.images.head.alt}/>
+          <img src={aside.images.head.src} alt={aside.images.head.alt} />
           <figcaption>{aside.images.head.title}</figcaption>
         </figure>
       </Fragment>
       <Fragment>
         <figure>
-          <img src={aside.images.pavskiy.src} alt={aside.images.pavskiy.alt}/>
+          <img src={aside.images.pavskiy.src} alt={aside.images.pavskiy.alt} />
           <figcaption>{aside.images.pavskiy.title}</figcaption>
         </figure>
       </Fragment>
-      {/*<Fragment>*/}
-      {/*  <figure>*/}
-      {/*    <img src={aside.bells.img.src} alt={aside.bells.img.alt}/>*/}
-      {/*    <figcaption>{aside.bells.title}</figcaption>*/}
-      {/*  </figure>*/}
-      {/*</Fragment>*/}
-      {/*<article>*/}
-      {/*  <img src={aside.images.invite.src}*/}
-      {/*       alt={aside.images.invite.alt}/>*/}
-      {/*</article>*/}
-      {/*<article>*/}
-      {/*  <h4>{aside.thanks.title}</h4>*/}
-      {/*  <span className='thanks'>{aside.thanks.text}</span>*/}
-      {/*  <img src={aside.thanks.img.src} alt={aside.thanks.img.alt}/>*/}
-      {/*</article>*/}
+      <article>
+        <img src={aside.charity_qr.img.src} alt={aside.charity_qr.img.alt} />
+      </article>
+      <article>
+        <img src={aside.charity_erip.img.src} alt={aside.charity_erip.img.alt} />
+      </article>
       <article>
         <h4>{aside.library.title}</h4>
         <span>{aside.library.text}</span>
