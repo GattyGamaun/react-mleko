12cb70295d8928f90f500b5ea205d4ca93fe6ddf
GattyGamaun <gattygamauneu@gmail.com>
2025-12-05 21:14:46 +0100

    refactor: change header info

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -2,17 +2,29 @@ import React from 'react';
 import './_styles.scss';
 import { Link } from 'react-router-dom';
 import Navigation from '../navigation';
+import data from '../../data/about.json';
+import photo from './img/divider-top.png';
 
 function Header() {
   return (
     <header>
       <div className="header">
-        <Link to={'/'}>
-          <h1>Млекопитательница</h1>
-        </Link>
-        <h2>Построим вместе красивый храм в красивом месте!</h2>
+        <div>
+          <div className="header__info">{data.contacts.name.patriarhate}</div>
+          <div className="header__info">{data.contacts.name.exarchate}</div>
+          <div className="header__info">{data.contacts.name.global}</div>
+          <div className="header__info">{data.contacts.name.episcopate}</div>
+        </div>
+        <img src={photo} alt="" />
+        <div>
+          <Link to={'/'}>
+            <div className="header__subtitle">Приход храма иконы Божией Матери</div>
+            <h1>"Млекопитательница"</h1>
+          </Link>
+          <div className="header__subtitle">д. Раубичи</div>
+        </div>
       </div>
-      <Navigation/>
+      <Navigation />
     </header>
   );
 }
